ae668cf07670cdcc95f66579246cae3193d8a296
Rohith Bollineni <Rohith@rojass-MacBook-Air.local>
2019-12-28 14:38:59 -0800

    init empty files

---
+++ gen.py
@@ -0,0 +1,21 @@
+import argparse
+import os
+import numpy as np
+import math
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        
+
